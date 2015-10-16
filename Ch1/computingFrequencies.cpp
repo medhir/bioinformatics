9aa9185efd7a750650e720dc9f97586ddd4364c3
Medhir Bhargava <medhirbhargava@gmail.com>
2015-10-16 11:38:51 -0400

    Finish number to sequence / sequence to number conversion

--- Ch1/computingFrequencies.cpp
+++ Ch1/computingFrequencies.cpp
@@ -2,16 +2,52 @@
 #include <string>
 #include <math.h>
 
-std::string NumToPattern(int num, int k)
+char NumToSymbol(int num) 
 {
-  std::string pattern;
-  //convert num to sequence, keeping in mind length k
-  return pattern;
+  if (num == 0) return 'A';
+  else if (num == 1) return 'C';
+  else if (num == 2) return 'G';
+  else if (num == 3) return 'T';
 }
+
+int SymbolToNum(char symbol) 
+{
+  if (symbol == 'A') return 0;
+  else if (symbol == 'C') return 1;
+  else if (symbol == 'G') return 2;
+  else if (symbol == 'T') return 3;
+  else return -1;
+}
+
+std::string NumToPattern(int index, int k)
+{
+  if (k == 1) 
+  {
+    std::string symbol = "";
+    symbol += NumToSymbol(index);
+    return symbol;
+  }
+
+  int prefixIndex = index/4;
+  int r = index - prefixIndex*4;
+  char symbol = NumToSymbol(r);
+
+  std::string PrefixPattern = NumToPattern(prefixIndex, k-1);
+  return PrefixPattern + symbol;
+}
+
 
 int PatternToNum(std::string pattern) 
 {
   //compute number based on sequence pattern
+  if(pattern.length() == 0) 
+  {
+    return 0; 
+  }
+
+  char symbol = pattern.back();
+  std::string prefix = pattern.substr(0, pattern.length()-1);
+  return 4 * PatternToNum(prefix) + SymbolToNum(symbol);
 }
 
 int computeFrequencies(std::string Text, int k) 
@@ -22,4 +58,11 @@ int computeFrequencies(std::string Text, int k)
   {
     frequencyArray[i] = 0;
   }
+}
+
+int main()
+{
+  int index, k; std::cin >> index >> k;
+  std::cout << NumToPattern(index, k) << std::endl;
+  return 0;
 }
